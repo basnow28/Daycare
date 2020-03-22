97e52075db7632983ca32f16af6b17c689883902
BiancaLSO <biancastan90@gmail.com>
2020-03-22 17:54:22 +0100

    Menus

--- Code/src/ui/ChildrenMenu.java
+++ Code/src/ui/ChildrenMenu.java
@@ -1,4 +1,78 @@
 package ui;
+import java.io.*;
 
-public class ChildrenMenu {
+// I remember we talked about having the delete and update in the search option, so they only need to be connected
+// same goes for the other menus
+public class ChildrenMenu{
+
+    public ChildrenMenu(){}
+
+    public static void displayChildrenMenu() throws FileNotFoundException {
+        System.out.println("\t\t\t -MANAGE CHILDREN MENU- \t\t\t");
+        System.out.println("1. Display information on children");
+        System.out.println("2. Add child from the Waiting list to the quarter");
+        System.out.println("3. Search for a child");
+        System.out.println("4. Exit");
+    }
+
+    public static void displaySearchChildMenu() throws FileNotFoundException {
+        System.out.println("Do you wish to perform further operations such as: ");
+        System.out.println("1. Update the child's information");
+        System.out.println("2. Remove the child from the quarter");
+        System.out.println("3. Exit");
+    }
+
+    public static void childrenMenu() throws FileNotFoundException {
+        String choice = "-1";
+        do {
+            displayChildrenMenu();
+
+            switch (choice) {
+                case "1":
+                    //MainMenu.printEmptyLines();
+                    //App.getController().displayChild();
+                    //MainMenu.printEmptyLines();
+                    break;
+
+                case "2":
+                    //MainMenu.printEmptyLines();
+                    //App.getController().addChildToQuarter()
+                    //MainMenu.printEmptyLines();
+                    break;
+
+                case "3":
+                    //MainMenu.printEmptyLines();
+                    //App.getController().searchChild();
+                    //MainMenu.printEmptyLines();
+                    break;
+
+                case "4":
+                    break;
+            }
+        } while (!choice.equals("5"));
+    }
+
+    public static void searchChildMenu() throws FileNotFoundException {
+        String choice = "-1";
+        do {
+            displaySearchChildMenu();
+
+            switch (choice) {
+                case "1":
+                    //MainMenu.printEmptyLines();
+                    //App.getController().updateChild();
+                    //MainMenu.printEmptyLines();
+                    break;
+
+                case "2":
+                    //MainMenu.printEmptyLines();
+                    //App.getController().removeChild();
+                    //MainMenu.printEmptyLines();
+                    break;
+
+                case "3":
+                    break;
+            }
+        } while (!choice.equals("4"));
+    }
 }
--- Code/src/ui/EmployeeMenu.java
+++ Code/src/ui/EmployeeMenu.java
@@ -1,5 +1,76 @@
 package ui;
+import java.io.*;
 
 public class EmployeeMenu{
 
+    public EmployeeMenu() {}
+
+    public static void displayEmployeeMenu() throws FileNotFoundException {
+        System.out.println("\t\t\t -MANAGE EMPLOYEE MENU- \t\t\t");
+        System.out.println("1. Display information on employees");
+        System.out.println("2. Add a new employee ");
+        System.out.println("3. Search for an employee");
+        System.out.println("4. Exit");
+    }
+
+    public static void displaySearchEmployeeMenu() throws FileNotFoundException {
+        System.out.println("Do you wish to perform further operations such as: ");
+        System.out.println("1. Update the employee's information");
+        System.out.println("2. Remove the employee");
+        System.out.println("3. Exit");
+    }
+
+    public static void employeeMenu() throws FileNotFoundException {
+        String choice = "-1";
+        do {
+            displayEmployeeMenu();
+
+            switch (choice) {
+                case "1":
+                    //MainMenu.printEmptyLines();
+                    //App.getController().displayEmployee();
+                    //MainMenu.printEmptyLines();
+                    break;
+
+                case "2":
+                    //MainMenu.printEmptyLines();
+                    //App.getController().addEmployee();
+                    //MainMenu.printEmptyLines();
+                    break;
+
+                case "3":
+                    //MainMenu.printEmptyLines();
+                    //App.getController().searchEmployee();
+                    //MainMenu.printEmptyLines();
+                    break;
+
+                case "4":
+                    break;
+            }
+        } while (!choice.equals("5"));
+    }
+
+    public static void searchEmployeeMenu() throws FileNotFoundException {
+        String choice = "-1";
+        do {
+            displaySearchEmployeeMenu();
+
+            switch (choice) {
+                case "1":
+                    //MainMenu.printEmptyLines();
+                    //App.getController().updateEmployee();
+                    //MainMenu.printEmptyLines();
+                    break;
+
+                case "2":
+                    //MainMenu.printEmptyLines();
+                    //App.getController().removeEmployee();
+                    //MainMenu.printEmptyLines();
+                    break;
+
+                case "3":
+                    break;
+            }
+        } while (!choice.equals("4"));
+    }
 }
